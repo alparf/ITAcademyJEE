61f82ad646cefbe8562aae8431f062c635eabc07
alparf <a.l.parfiankou@gmail.com>
2020-12-22 14:08:29 +0300

    remove bad code (imho)

--- app/src/main/java/by/academy/controller/LoginController.java
+++ app/src/main/java/by/academy/controller/LoginController.java
@@ -1,15 +1,12 @@
 package by.academy.controller;
 
-import by.academy.dao.impl.UserInMemory;
 import by.academy.exception.UserNotFoundException;
 import by.academy.model.bean.User;
-import by.academy.model.bean.UserType;
 import by.academy.service.IUserService;
 import by.academy.service.impl.UserService;
 
 import static by.academy.constant.FileConstant.*;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,28 +17,8 @@ import static by.academy.constant.SessionConstant.*;
 
 public class LoginController extends AbstractController {
 
-    private static volatile User admin;
-
-    private static User getAdmin(String userName, String password) {
-        User user = admin;
-        if(user == null) {
-            synchronized (LoginController.class) {
-                user = admin;
-                if(user == null) {
-                    user = admin = new User();
-                    admin.setUserName(userName);
-                    admin.setPassword(password);
-                }
-            }
-        }
-        return user;
-    }
-
     @Override
     public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        if(null == admin) {
-            initAdmin();
-        }
         String userName = req.getParameter(USER_NAME);
         String password = req.getParameter(PASSWORD);
         IUserService service = new UserService();
@@ -57,11 +34,14 @@ public class LoginController extends AbstractController {
         res.sendRedirect(HOME);
     }
 
-    private void initAdmin() {
+    @Override
+    public void init() throws ServletException {
+        User admin = new User();
         String adminName = getServletConfig().getInitParameter(ADMIN_NAME);
         String adminPassword = getServletConfig().getInitParameter(ADMIN_PASSWORD);
+        admin.setUserName(adminName);
+        admin.setPassword(adminPassword);
         UserService service = new UserService();
-        getAdmin(adminName, adminPassword);
         service.addUser(admin);
     }
 }
