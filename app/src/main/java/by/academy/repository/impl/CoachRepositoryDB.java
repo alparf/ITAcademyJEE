ef23fac6bd9614966acc2bd52e427c8db064d1d4
alparf <A4cdi92ud@>
2021-01-09 19:20:41 +0300

    1

---
+++ app/src/main/java/by/academy/repository/impl/CoachRepositoryDB.java
@@ -0,0 +1,34 @@
+package by.academy.repository.impl;
+
+import by.academy.model.bean.Coach;
+import by.academy.repository.ICoachRepository;
+import by.academy.specification.ICoachSpecification;
+
+import java.util.List;
+
+public class CoachRepositoryDB implements ICoachRepository {
+    @Override
+    public void addCoach(Coach coach) {
+
+    }
+
+    @Override
+    public void removeCoach(Coach coach) {
+
+    }
+
+    @Override
+    public void setCoach(Coach coach) {
+
+    }
+
+    @Override
+    public void addSalary(Coach coach, int salary) {
+
+    }
+
+    @Override
+    public List<Coach> query(ICoachSpecification specification) {
+        return null;
+    }
+}
