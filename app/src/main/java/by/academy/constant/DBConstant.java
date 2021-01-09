0ba2dbb133a08bb8b0a03e40c107d62b7c51d260
alparf <a.l.parfiankou@gmail.com>
2021-01-09 23:56:56 +0300

    connection pool

--- app/src/main/java/by/academy/constant/DBConstant.java
+++ app/src/main/java/by/academy/constant/DBConstant.java
@@ -2,6 +2,6 @@ package by.academy.constant;
 
 public class DBConstant {
     public static final String URL = "jdbc:postgresql://127.0.0.1:5432/itacademydb";
-    public static final String USER = "postgres";
-    public static final String PASSWORD = "postgres";
+    public static final String USER = "itacademy";
+    public static final String PASSWORD = "itacademy";
 }
