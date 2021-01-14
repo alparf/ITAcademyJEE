a3461652a6ea5295dbae8dfb32f8d4e55c15dc5e
alparf <a.l.parfiankou@gmail.com>
2021-01-14 16:04:01 +0300

    refactor spec

--- app/src/main/java/by/academy/specification/ISqlSpecification.java
+++ app/src/main/java/by/academy/specification/ISqlSpecification.java
@@ -4,6 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public interface ISqlSpecification extends IUserSpecification {
+public interface ISqlSpecification {
     PreparedStatement getPreparedStatement(Connection connection) throws SQLException;
 }
