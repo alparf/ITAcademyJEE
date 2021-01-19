9df08118e71c9800d2aae3bbf14216787760aec3
alparf <A4cdi92ud@>
2021-01-19 12:36:55 +0300

    add json filter

---
+++ app/src/main/java/by/academy/filter/JsonFilter.java
@@ -0,0 +1,17 @@
+package by.academy.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter("/AverageSalariesController")
+public class JsonFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+            throws IOException, ServletException {
+        servletRequest.setCharacterEncoding("UTF-8");
+        servletResponse.setContentType("text/json");
+        chain.doFilter(servletRequest, servletResponse);
+    }
+}
