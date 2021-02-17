78b30df686c1c84e3e044dadab14f3a23c153a67
alparf <A4cdi92ud@>
2021-02-17 11:43:46 +0300

    hibernate

--- model/src/main/java/by/academy/model/bean/AbstractEntity.java
+++ model/src/main/java/by/academy/model/bean/AbstractEntity.java
@@ -4,17 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class AbstractEntity implements Serializable {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id")
     private Long id;
 }
